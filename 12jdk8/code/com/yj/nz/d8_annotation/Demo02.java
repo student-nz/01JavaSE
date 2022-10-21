2ed999204deb1c38b67ed14b3b760340655fda96
“itcast” <hello@itcast.cn>
2022-10-21 20:17:45 +0800

    大意了，jdk8还是有点难度，抓紧补数据结构与算法攻完恋上

---
+++ 12jdk8/code/com/yj/nz/d8_annotation/Demo02.java
@@ -0,0 +1,23 @@
+package com.yj.nz.d8_annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+public class Demo02 <@TypeParam T> {
+    private @NotNull int a = 10;
+
+    public void test(@NotNull String str, @NotNull int a) {
+        @NotNull double d = 10.1;
+    }
+
+    public <@TypeParam E extends Integer> void test01() {
+    }
+}
+
+@Target(ElementType.TYPE_USE)
+@interface NotNull {
+}
+
+@Target(ElementType.TYPE_PARAMETER)
+@interface TypeParam {
+}
