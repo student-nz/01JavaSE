518347fb64fa2379cbd81ceaac27980c09aa3ef2
“itcast” <hello@itcast.cn>
2022-12-08 08:41:16 +0800

    push

--- 09thread_base/code/com/yj/nz/d2_api/currentthread/CurrentthreadDemo.java
+++
@@ -1,8 +0,0 @@
-package com.yj.nz.d2_api.currentthread;
-
-public class CurrentthreadDemo {
-    public static void main(String[] args) {
-        String name = Thread.currentThread().getName();
-        System.out.println(name);
-    }
-}
